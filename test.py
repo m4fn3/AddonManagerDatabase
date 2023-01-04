cb9bb8a4f54c974223a7f8a96544c6d007f1e1f2
mafusuke <mafusukee@gmail.com>
2023-01-05 00:41:30 +0900

    test

--- test.py
+++
@@ -1,6 +0,0 @@
-import requests
-import re
-version_pattern = re.compile(r'version:\s*["\']([0-9.]+)["\']')
-r = requests.get("https://raw.githubusercontent.com/6days9weeks/EnableStaging/mistress/dist/EnableStaging.js")
-res = version_pattern.findall(r.text)
-print(res)
