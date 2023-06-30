4f8cf197cffb4d4bf2b8975647c571f922ceeb1d
mafu <mafusukee@gmail.com>
2023-07-01 01:56:28 +0900

    added last-updated-date and a feature to restore old meta data with english comments on every function!

--- updater_v2.py
+++ updater_v2.py
@@ -1,27 +1,30 @@
 import copy
+import datetime
 from datetime import timedelta
-
-import discord
+from typing import List, Union
+import selfcord
 import requests
 import traceback2
-from discord.ext import commands, tasks
+from selfcord.ext import commands, tasks
 from dotenv import load_dotenv
 import os
 import re
 import time
 from tqdm import tqdm
 import json
+import demjson
 
 # 環境変数読込
 load_dotenv(verbose=True)
 load_dotenv(os.path.join(os.path.dirname(__file__), ".env"))
 
 # Client作成
-bot = commands.Bot("!", self_bot=True, intents=discord.Intents.default())
+bot = commands.Bot("!", self_bot=True)
 
 # 設定
 debug = False
-dump_all = False
+do_dump_all = False
+get_last_updated = True
 
 # データ
 channel_ids = {
@@ -47,9 +50,7 @@ title_pattern = re.compile(r"^\*\*.*?\n\n")
 title2_pattern = re.compile(r"^\*\*.*?\n")
 command_pattern = re.compile(r"</(.+?):1>")
 desc_pattern1 = re.compile(r"{name:[a-zA-Z0-9$_]+,.*?description:([a-zA-Z0-9$_]+)")  # <一般的なパターン> 1~2文字の略された変数で、その変数の指す値をとりに行く
-# desc_pattern2 = re.compile(r'{name:"[a-zA-Z_]+",.*?description:["\'](.*?)["\']')  # <特殊なパターン> 直接文字列が指定されている
 desc_pattern2 = re.compile(r'{name:"[a-zA-Z_]+",.*?description:(?:"(.*?)"|\'(.*?)\')')  # <特殊なパターン> 直接文字列が指定されている
-# desc_pattern3 = re.compile(r'description:\s*["\'](.*?)["\']')  # <超特殊なパターン> minifyされていない場合
 desc_pattern3 = re.compile(r'description:\s*(?:"(.*?)"|\'(.*?)\')')  # <超特殊なパターン> minifyされていない場合
 variable_pattern = r'=["\'](.*?)["\'],'  # TODO: 最後の,は必要か不明
 variable_pattern_list = r'=(\[.*?\]),'
@@ -61,6 +62,7 @@ color_pattern1 = re.compile(r"{name:[a-zA-Z0-9$_]+,.*?color:([a-zA-Z0-9$_]+)")
 color_pattern2 = re.compile(r'{name:"[a-zA-Z_]+",.*?color:"([0-9a-fA-F#]+)"')
 author_pattern1 = re.compile(r"{name:[a-zA-Z0-9$_]+,.*?authors:([a-zA-Z0-9$_]+)")
 author_pattern2 = re.compile(r'{name:"[a-zA-Z_]+",.*?authors:(\[.*?\])')
+raw_gh_pattern = re.compile(r'https?://raw\.githubusercontent\.com/(?P<user>[\w_\-]+)/(?P<repo>[\w_\-]+)/(?P<branch>[\w_\-]+)/(?P<path>.*)')
 
 # 独自設定
 exclude_addons = {
@@ -83,7 +85,7 @@ cached = {}  # message_id: timestamp
 
 
 # GitHubに更新を反映
-def push_changes(addon_type, log="No Log"):
+def push_changes(addon_type: str, log: str = "No Log"):
     if not debug:
         t = str(time.time())
 
@@ -101,11 +103,14 @@ def pull_changes():
 
 
 @bot.event  # 起動時
-async def on_ready():
+async def on_ready() -> None:
+    """
+    when discord.py is ready
+    """
     print(f"[*] Logged in as {bot.user}")
     pull_changes()
     for addon_type in ["plugin", "theme"]:
-        if dump_all:
+        if do_dump_all:
             await dump_all(addon_type)
         async for message in bot.get_channel(channel_ids[addon_type]).history(limit=None):
             cached[message.id] = message.created_at
@@ -114,16 +119,21 @@ async def on_ready():
 
 
 @bot.event  # 送信検知
-async def on_message(message):
+async def on_message(message: selfcord.Message) -> None:
+    """
+    detect new messages in the channel of either plugins or themes and add them to the database
+    :param message: a message we received
+    :return:
+    """
     # dumpの時は必ず変更があるので常に更新してpushする
     if message.channel.id == channel_ids["plugin"]:
         cached[message.id] = message.created_at  # 指定チャンネルでの送信はtimestampをキャッシュしておく
         if list(set(js_pattern.findall(message.content))):
-            await add("plugin", message)
+            await upsert("plugin", message)
     elif message.channel.id == channel_ids["theme"]:
         cached[message.id] = message.created_at
         if list(set(json_pattern.findall(message.content))):
-            await add("theme", message)
+            await upsert("theme", message)
 
 
 # @bot.event
@@ -137,65 +147,123 @@ async def on_message(message):
 #                 await add("theme", message, True)
 
 # メッセージ取得
-async def get_message(channel_id, message_id):
+async def get_message(channel_id: int, message_id: int) -> Union[selfcord.Message, None]:
+    """
+    get the specific message with channel id & message id
+    :param channel_id:
+    :param message_id:
+    :return: the message if it exists
+    """
     messages = [message async for message in bot.get_channel(channel_id).history(after=cached[message_id] - timedelta(seconds=1), before=cached[message_id] + timedelta(seconds=1))]
     return messages[0] if messages else None
 
 
 @bot.event  # 編集検知
-async def on_raw_message_edit(payload):
+async def on_raw_message_edit(payload: selfcord.RawMessageUpdateEvent) -> None:
+    """
+    detect edits of messages in either plugins or themes channel to update detailed plugin descriptions
+    :param payload:
+    """
     if "content" in payload.data:  # embed更新などでも呼ばれるので
         if payload.channel_id == channel_ids["plugin"]:
             # message = await bot.get_channel(payload.channel_id).fetch_message(payload.message_id) # bot専用
             # fetch_messageはbot専用なのでtimestampで絞ってキャッシュ外のメッセージの取得に代用
             if message := await get_message(payload.channel_id, payload.message_id):
                 if list(set(js_pattern.findall(message.content))):
-                    await add("plugin", message, True)
+                    await upsert("plugin", message, True)
         elif payload.channel_id == channel_ids["theme"]:
             if message := await get_message(payload.channel_id, payload.message_id):
                 if list(set(json_pattern.findall(message.content))):
-                    await add("theme", message, True)
+                    await upsert("theme", message, True)
 
 
 # 1時間ごとに更新を確認する
 @tasks.loop(hours=1)
-async def loop():
+async def loop() -> None:
+    """
+    Check updates of all addons every hour
+    """
+    # pull remote changes before we make changes
     pull_changes()
+    # check updates respectively
     for addon_type in ["plugin", "theme"]:
         check_update(addon_type)
 
 
 # JavaScript形式の連想配列をプロパティ名を囲ってjson形式に変換
-def format_author_array(raw):
-    # TODO: とりあえずなんとかしているが本来は正規表現で対処すべき
-    formatted = raw.replace('name:', '"name":').replace('id:', '"id":').replace("profile:", '"profile":').replace("icon:", '"icon":')
+def format_author_array(raw: str) -> Union[list, None]:
+    """
+    Convert js-style dict to valid JSON format
+    :param raw:
+    :return:
+    """
     try:
-        json.loads(formatted)
-    except:  # とりあえず無視
+        # load js-style dict which keys doesn't have quotes
+        data = demjson.decode(raw)
+        # extract only name and id field (some has unique keys here)
+        data = [{"name": u["name"], "id": u["id"]} for u in data]
+    except:
+        print("-- format author error")
+        print(traceback2.format_exc())
         return None
     else:
-        return formatted
+        return data
 
 
 # $が変数名に使われていると困るのでエスケープする
-def escape_regex(t):
+def escape_regex(t: str) -> str:
+    """
+    escape specific characters to be used in regex
+    :param t: regex
+    :return: escaped regex
+    """
     return t.replace("$", r"\$")
 
 
 # URLからダウンロードして情報を解析する
-def fetch(addon_type, download_url):
+def fetch(addon_type, download_url, old_meta=None) -> Union[List[Union[str, dict]], None]:
+    """
+    fetch an raw addon code and parse the necessary data
+    :param addon_type: plugin or theme
+    :param download_url: install url of an addon
+    :param old_meta: (optional) old metadata of an addon
+    :return: [addon_name, addon_data] (return None if it fails to download/parse the code)
+    """
     if addon_type == "plugin":
         plugin = {}
-
+        # download the plugin
         plugin_name = download_url.split("/")[-1].replace(".js", "")
         plugin["url"] = download_url
         headers = {"Cache-Control": "no-cache", "Pragma": "no-cache"}
         try:
             r = requests.get(download_url, headers=headers)
 
+            # ignore plugins that return invalid code
             if len(r.text) <= 20 or ("<h1>404</h1>" in r.text and "<!DOCTYPE html>" in r.text):  # invalid or 404
                 return None
 
+            # get last updated time
+            if get_last_updated:
+                if "Last-Modified" in r.headers:  # 確認済み: github release link / selfcord attachment link
+                    plugin["last_update"] = int(time.mktime(datetime.datetime.strptime(r.headers["Last-Modified"], "%a, %d %b %Y %H:%M:%S GMT").timetuple()))
+                elif r.url.startswith("https://raw.githubusercontent.com/"):
+                    try:
+                        match = raw_gh_pattern.fullmatch(r.url)
+                        resp = requests.get(
+                            f"https://api.github.com/repos/{match.group('user')}/{match.group('repo')}/commits?path={match.group('path')}&page=1&per_page=1",
+                            headers={'Authorization': 'token ' + os.environ["GH_TOKEN"]}
+                        )
+                        data = resp.json()
+                        timestamp = data[0]["commit"]["committer"]["date"]
+                    except:
+                        print("--- regex error ---")
+                        print(data)
+                        print(traceback2.format_exc())
+                    else:
+                        plugin["last_update"] = int(time.mktime(datetime.datetime.strptime(timestamp, "%Y-%m-%dT%H:%M:%S%z").timetuple()))
+                        # print(plugin["last_update"])
+
+            # get description, version, author and color using regular expressions
             if res := desc_pattern1.findall(r.text):  # descriptionに対応する変数名を取得
                 res = re.findall(escape_regex(res[0]) + variable_pattern, r.text)  # 変数名に対応する値を取得
                 if res:
@@ -224,28 +292,39 @@ def fetch(addon_type, download_url):
             elif res := desc_pattern3.findall(r.text):  # descriptionを直接取得
                 plugin["description"] = res[0][0]
 
-            if "version" not in plugin:
+            if "version" not in plugin:  # rare case
                 if res := version_pattern_simple.findall(r.text):  # ""でバージョンが書いてあるパターン(2,3で普通はとれる)
                     plugin["version"] = res[0]
 
+            # restore missing fields with old data if any
+            if old_meta:
+                keys = ["description", "version", "color", "author", "last_update"]
+                for k in keys:
+                    if (k not in plugin) and (k in old_meta):
+                        plugin[k] = old_meta[k]
+
             return [plugin_name, plugin]
         except:
-            print("------ Error on parsing a plugin -----------")
+            print("------ Error in parsing a plugin -----------")
             print(download_url)
             print(traceback2.format_exc())
             return None
 
     elif addon_type == "theme":
+        # download the theme
         headers = {'Accept': 'application/json', "Cache-Control": "no-cache", "Pragma": "no-cache"}
         try:
             r = requests.get(download_url, headers=headers)
             theme = {}
             res = r.json()
             theme["url"] = download_url
+            # get description, version and author using regular expressions
             if "description" in res:
                 theme["description"] = res["description"]
-            # if "authors" in res:
-            #     theme["authors"] = res["authors"]
+            if "authors" in res:
+                theme["author"] = res["authors"]
+            if "color" in res:
+                theme["color"] = res["color"]
             if "version" in res:
                 if version_pattern_raw.fullmatch(res["version"]):  # 適当な文字列を入れているふざけたやつが含まれるので除く！
                     theme["version"] = res["version"]
@@ -294,12 +373,20 @@ def extract_preview(message, addon_type):
 
 
 # messageから新規追加
-async def add(addon_type, message, is_edit=False):
+async def upsert(addon_type: str, message: selfcord.Message, is_edit: bool = False) -> None:
+    """
+    add or update the plugin with a message
+    :param addon_type: either plugin or theme
+    :param message: a message that includes addon information
+    :param is_edit:
+    """
+    # is_editがFalseの場合は新規追加 (on_messageより)
+    # is_editがTrueの場合は更新 (on_raw_message_editより)
     try:
         download_url_pattern = js_pattern if addon_type == "plugin" else json_pattern
         result = list(set(download_url_pattern.findall(message.content)))
         download_url = result[0]
-        if download_url in exclude_addons["plugin"]:
+        if download_url in exclude_addons["plugin"]:  # 除外リストにある場合は除く
             return
         with open(f"{addon_type}s.json", "r", encoding="utf-8") as f:
             data = json.load(f)
@@ -319,7 +406,12 @@ async def add(addon_type, message, is_edit=False):
 
 
 # 全てをダンプする
-async def dump_all(addon_type):
+async def dump_all(addon_type: str):
+    """
+    dump all addons and force it to override existing data
+    :param addon_type:
+    :return:
+    """
     try:
         data = {}
         channel = bot.get_channel(channel_ids[addon_type])
@@ -354,7 +446,12 @@ async def dump_all(addon_type):
 
 
 # 既存のアドオンの更新を確認して情報を更新する
-def check_update(addon_type):
+def check_update(addon_type: str):
+    """
+    check updates of all addons of the specific addon type
+    :param addon_type: either plugin or theme
+    :return: if there were updates
+    """
     with open(f"{addon_type}s.json", "r", encoding="utf-8") as f:
         data = json.load(f)
     data_old = copy.deepcopy(data)  # コピー後のデータも変わってしまうのを防ぐ
@@ -362,15 +459,10 @@ def check_update(addon_type):
     updated = []
     for name in tqdm(names):
         download_url = data[name]["url"]
-        if res := fetch(addon_type, download_url):
+        if res := fetch(addon_type, download_url, data[name]):
             name_, meta = res
-            # descやverは削除されていても前はあった場合は昔のを残しておく
-            if ("description" not in meta) and ("description" in data[name_]):
-                meta["description"] = data[name_]["description"]
-            if ("version" not in meta) and ("version" in data[name_]):
-                meta["version"] = data[name_]["version"]
             data[name_] = meta
-            if data_old[name] != meta:
+            if data_old[name] != meta:  # 何らかの変更があった場合は更新リストに追加
                 updated.append(name)
     if data_old != data:
         with open(f"{addon_type}s.json", "w", encoding="utf-8") as f:
@@ -383,4 +475,4 @@ def check_update(addon_type):
         return False
 
 
-bot.run(os.getenv("TOKEN"), bot=False)
+bot.run(os.getenv("TOKEN"))
