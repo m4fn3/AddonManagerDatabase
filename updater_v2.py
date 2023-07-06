db44e6a248209caaac4046da138a205ffcadc8cb
mafu <43488869+m4fn3@users.noreply.github.com>
2023-07-06 15:21:37 +0900

    Added TrackEdit

--- updater_v2.py
+++ updater_v2.py
@@ -76,7 +76,8 @@ exclude_addons = {
 include_addons = {
     "plugin": [
         "https://raw.githubusercontent.com/xenos1337/enmity-plugins/main/ReactDevTools/dist/ReactDevTools.js",
-        "https://raw.githubusercontent.com/m4fn3/FixVoiceMessageCrash/master/dist/FixVoiceMessageCrash.js"
+        "https://raw.githubusercontent.com/m4fn3/FixVoiceMessageCrash/master/dist/FixVoiceMessageCrash.js",
+        "https://raw.githubusercontent.com/m4fn3/TrackEdit/master/dist/TrackEdit.js"
     ],
     "theme": []
 }
